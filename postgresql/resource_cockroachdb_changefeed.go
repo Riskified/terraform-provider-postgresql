5cba56f77c2244f51f1afedcbb9b35d32479674b
Yoav Shemesh <yoav.shemesh@riskified.com>
2025-03-18 12:26:30 +0200

    create cdc, phase 2

--- postgresql/resource_cockroachdb_changefeed.go
+++ postgresql/resource_cockroachdb_changefeed.go
@@ -8,14 +8,11 @@ import (
 )
 
 const (
-	CDCtableList        = "table_list"
-	CDCKafkaUser        = "kafka_user"
-	CDCKafkaPass        = "kafka_password"
-	CDCTopicPrefix      = "topic_prefix"
-	CDCAvroSchemaPrefix = "avro_schema_prefix"
-	CDCKafkaUrl         = "kafka_url"
-	CDCRegistryUrl      = "registry_url"
-	CDCStartFrom        = "start_from"
+	CDCtableList              = "table_list"
+	CDCKafkaConnectionName    = "kafka_connection_name"
+	CDCAvroSchemaPrefix       = "avro_schema_prefix"
+	CDCRegistryConnectionName = "registry_connection_name"
+	CDCStartFrom              = "start_from"
 )
 
 func resourceCockroachDBChangefeed() *schema.Resource {
@@ -37,26 +34,11 @@ func resourceCockroachDBChangefeed() *schema.Resource {
 				Elem:        &schema.Schema{Type: schema.TypeString},
 				Description: "Sets the tables list to create the changefeed for",
 			},
-			CDCKafkaUser: {
+			CDCKafkaConnectionName: {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
 				Description: "kafka user name",
-			},
-			CDCKafkaPass: {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				ForceNew:    true,
-				Description: "Sets kafka password",
-			},
-			CDCTopicPrefix: {
-				Type:         schema.TypeString,
-				Optional:     true,
-				Computed:     true,
-				ForceNew:     true,
-				Description:  "Sets the topic prefix",
-				ValidateFunc: validation.StringIsNotEmpty,
 			},
 			CDCAvroSchemaPrefix: {
 				Type:         schema.TypeString,
@@ -65,14 +47,7 @@ func resourceCockroachDBChangefeed() *schema.Resource {
 				ValidateFunc: validation.StringIsNotEmpty,
 				Description:  "avro schema prefix",
 			},
-			CDCKafkaUrl: {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringIsNotEmpty,
-				Description:  "kafka url",
-			},
-			CDCRegistryUrl: {
+			CDCRegistryConnectionName: {
 				Type:         schema.TypeString,
 				Required:     true,
 				ForceNew:     true,
@@ -91,35 +66,16 @@ func resourceCockroachDBChangefeed() *schema.Resource {
 
 func resourceCockroachDBChangefeedCreate(db *DBConnection, d *schema.ResourceData) error {
 	tableListInterface := d.Get(CDCtableList).([]interface{})
-	kafkaUser := d.Get(CDCKafkaUser).(string)
-	kafkaPass := d.Get(CDCKafkaPass).(string)
-	topicPrefix := d.Get(CDCTopicPrefix).(string)
+	kafkaConnectionName := d.Get(CDCKafkaConnectionName).(string)
+	registryConnectionName := d.Get(CDCRegistryConnectionName).(string)
 	avroSchemaPrefix := d.Get(CDCAvroSchemaPrefix).(string)
-	kafkaUrl := d.Get(CDCKafkaUrl).(string)
-	registryUrl := d.Get(CDCRegistryUrl).(string)
+
 	startFrom := d.Get(CDCStartFrom).(string)
 
 	database := db.client.databaseName
 	txn, err := startTransaction(db.client, database)
 	if err != nil {
 		return fmt.Errorf("Error starting transaction: %w", err)
-	}
-
-	sqlConfluentRegistry := fmt.Sprintf("CREATE EXTERNAL CONNECTION confluent_registry_%s AS \"https://%s\";", avroSchemaPrefix, registryUrl)
-	if _, err := txn.Exec(sqlConfluentRegistry); err != nil {
-		return fmt.Errorf("Error creating EXTERNAL CONNECTION confluent_registry: %w", err)
-	}
-	if err = txn.Commit(); err != nil {
-		return fmt.Errorf("could not commit transaction: %w", err)
-	}
-
-	txn, err = startTransaction(db.client, database)
-	sqlKafka := fmt.Sprintf("CREATE EXTERNAL CONNECTION kafka_%s AS \"kafka://%s?topic_prefix=%s_&tls_enabled=true&sasl_enabled=true&sasl_user=%s&sasl_password=%s&sasl_mechanism=PLAIN\"", topicPrefix, kafkaUrl, topicPrefix, kafkaUser, kafkaPass)
-	if _, err := txn.Exec(sqlKafka); err != nil {
-		return fmt.Errorf("Error creating EXTERNAL CONNECTION kafka: %w", err)
-	}
-	if err = txn.Commit(); err != nil {
-		return fmt.Errorf("could not commit transaction: %w", err)
 	}
 
 	var cursorClause string
@@ -133,8 +89,8 @@ func resourceCockroachDBChangefeedCreate(db *DBConnection, d *schema.ResourceDat
 	}
 	tableListStr := strings.Join(tableList, ", ")
 	sqlChangefeed := fmt.Sprintf(
-		`CREATE CHANGEFEED FOR TABLE %v INTO "external://kafka_%s" WITH updated, %s diff, on_error='pause', format = avro, avro_schema_prefix='%s_', confluent_schema_registry = 'external://confluent_registry_%s'`,
-		tableListStr, topicPrefix, cursorClause, avroSchemaPrefix, avroSchemaPrefix,
+		`CREATE CHANGEFEED FOR TABLE %v INTO "external://%s" WITH updated, %s diff, on_error='pause', format = avro, avro_schema_prefix='%s_', confluent_schema_registry = 'external://%s'`,
+		tableListStr, kafkaConnectionName, cursorClause, avroSchemaPrefix, registryConnectionName,
 	)
 	txn, err = startTransaction(db.client, database)
 	result, err := txn.Exec(sqlChangefeed)
@@ -149,8 +105,11 @@ func resourceCockroachDBChangefeedCreate(db *DBConnection, d *schema.ResourceDat
 	rowsAffected, err := result.RowsAffected()
 	return fmt.Errorf("rows affected %w", rowsAffected)
 
-	d.Set(CDCAvroSchemaPrefix, fmt.Sprintf("confluent_registry_%s", avroSchemaPrefix))
-	d.Set(CDCTopicPrefix, fmt.Sprintf("kafka_%s", topicPrefix))
+	d.SetId("1")
+	d.Set(CDCAvroSchemaPrefix, fmt.Sprintf("%s_", avroSchemaPrefix))
+	d.Set(CDCRegistryConnectionName, registryConnectionName)
+	d.Set(CDCKafkaConnectionName, kafkaConnectionName)
+
 	//// Return the number of rows affected as a string
 	return nil
 }
